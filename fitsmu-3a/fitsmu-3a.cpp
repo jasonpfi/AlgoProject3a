bcb02abfc0e197823bd517036d2432fcda1aa4af
Jason Fitch <jasonpfitch@gmail.com>
2018-10-25 10:37:49 -0500

    more comments

--- fitsmu-3a/fitsmu-3a.cpp
+++ fitsmu-3a/fitsmu-3a.cpp
@@ -6,6 +6,7 @@
 
 void search(const dictionary& d);
 
+// Main method
 int main()
 {
     std::cout << "Word Search Program" << std::endl;
@@ -15,12 +16,17 @@ int main()
 	dictionary wordList(wordFile);
 	wordFile.close();
 
+   // Search dictionary
    search(wordList);
 
 	std::cin.get();
 	return 0;
-}
+} // End main
 
+// Search the dictionary with every possible word greater than 5 characters in
+//  the grid
+//
+//  dictionary d: sorted dictionary object to be searched
 void search(const dictionary& d)
 {
 
@@ -30,17 +36,20 @@ void search(const dictionary& d)
 
 	std::ifstream f(("data/" + fileName).c_str());
 
+   // Instantiate grid object
 	grid g(f);
 
 	f.close();
 
+   // Get all of the possible words from the grid
    std::vector<std::string> candidateWords(g.readGrid());
 
    std::cout << "Words in the grid found in the dictionary: " << std::endl;
 
+   // Iterate through the vector of possible words. If the word is found in
+   //  the dictionary, print it to the screen
    std::vector<std::string>::iterator it;
    int printIndex = 0;
-
    for (it = candidateWords.begin(); it != candidateWords.end(); it++)
    {
       if (d.binarySearch(*it) != -1)
@@ -54,4 +63,4 @@ void search(const dictionary& d)
    }
 
    std::cout << std::endl;
-}
+} // End Search
